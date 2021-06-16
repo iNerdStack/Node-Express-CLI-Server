cbb8b93a1b017a87813bdf5b0773b3f39dd14dda
inerdstack <isaacajetunmobi@gmail.com>
2021-06-16 20:01:52 +0100

    added support for linux

--- app.ts
+++ app.ts
@@ -5,32 +5,36 @@ const jsonParser = express.json();
 const PORT = 4000;
 
 //crypto-CLI hashTypes
-enum hashTypes  { sha1= "sha1", sha512 = "sha512",md4 =  "md4",md5 =  "md5" }
+enum hashTypes { sha1 = "sha1", sha512 = "sha512", md4 = "md4", md5 = "md5" }
 
 let hashedPassword: string = "";
 
 app.post("/hash", jsonParser, (req, res, next) => {
-    //execute CLI Dependency in cli-dependencies folder
-    const exec = spawn(`${__dirname}\\cli-dependencies\\crypto-cli`, [hashTypes.md5 , `--text=${req.body.password}`], );
+  //execute CLI Dependency in cli-dependencies folder
 
-    exec.stdout.on('data', (data) => {
-        hashedPassword = data.toString().replace(/[\n\r]/g, "");
-    });
-    
-    exec.stderr.on('data', (data) => {
-       // console.error(`stderr: ${data}`);
-      //if terminal error return  error directly from terminal
-      res.status(500).json(data);
-    });
-    
-    exec.on('close', (code) => {
+  //windows execute code
+  //const exec = spawn(`${__dirname}\\cli-dependencies\\crypto-cli`, [hashTypes.md5, `--text=${req.body.password}`],);
+  //linux execute code 
+  const exec = spawn('cli-dependencies/crypto-cli', [hashTypes.md5, `--text=${req.body.password}`],);
+
+  exec.stdout.on('data', (data) => {
+    hashedPassword = data.toString().replace(/[\n\r]/g, "");
+  });
+
+  exec.stderr.on('data', (data) => {
+    // console.error(`stderr: ${data}`);
+    //if terminal error return  error directly from terminal
+    res.status(500).json(data);
+  });
+
+  exec.on('close', (code) => {
     // console.log(`child process exited with code ${code}`);
     //exit code can be used as status code for cli operations
     res.status(200).json({
-        userName: req.body.userName,
-        hashedPassword
+      userName: req.body.userName,
+      hashedPassword
     });
-    });
+  });
 
 });
 
