de1b7bc6e87848cf2c5cd9d3164ecfff8648ef7c
inerdstack <isaacajetunmobi@gmail.com>
2021-06-19 00:28:36 +0100

    Added Custom Wrapper For CLI-Modules

--- cli-modules/native/exec.ts
+++ cli-modules/native/exec.ts
@@ -3,22 +3,22 @@ import os from 'os';
 
 
 export async function executeCommand(module: string, command: string, flag: string): Promise<string> {
-    return new Promise(function (resolve, reject) {
-        let exec;
-        if (os.platform() === "win32") {
-            //windows execute code
-          exec = spawn(`${process.cwd()}\\cli-modules\\${module}\\${module}`, [command, flag ],);
-         }
-         else {
-           //linux execute code 
-           exec = spawn(`../cli-modules/${module}/${module}`,[command, flag],);
-         }
-        exec.stdout.on('data', (data) => { 
-          resolve(data.toString().replace(/[\n\r]/g, ""));
-        });
-        exec.on('error', function (err) {
-          reject(err);
-        });
-      })
+  return new Promise(function (resolve, reject) {
+    let exec;
+    if (os.platform() === "win32") {
+      //windows execute code
+      exec = spawn(`${process.cwd()}\\cli-modules\\${module}\\${module}`, [command, flag],);
+    }
+    else {
+      //linux execute code 
+      exec = spawn(`${process.cwd()}/cli-modules/${module}/${module}`, [command, flag],);
+    }
+    exec.stdout.on('data', (data) => {
+      resolve(data.toString().replace(/[\n\r]/g, ""));
+    });
+    exec.on('error', function (err) {
+      reject(err);
+    });
+  })
 }
 
